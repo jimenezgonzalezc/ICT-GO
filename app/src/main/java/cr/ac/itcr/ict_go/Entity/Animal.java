7699974ba97d6493068307c790a8abb74900a683
jimenezgonzalezc <car_ed24@hotmail.es>
2016-04-12 23:58:02 -0600

    List animals

---
+++ app/src/main/java/cr/ac/itcr/ict_go/Entity/Animal.java
@@ -0,0 +1,63 @@
+package cr.ac.itcr.ict_go.Entity;
+
+/**
+ * Created by car_e on 4/12/2016.
+ */
+public class Animal {
+    private int id;
+    private String name;
+    private String scientificName;
+    private String description;
+    private String idImg;
+
+
+    public Animal(int id, String n, String sn, String d, String img){
+        this.id = id;
+        this.name = n;
+        this.scientificName = sn;
+        this.description = d;
+        this.idImg = img;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIdImg() {
+        return idImg;
+    }
+
+    public void setIdImg(String idImg) {
+        this.idImg = idImg;
+    }
+}
